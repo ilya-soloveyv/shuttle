bd5fe61f6654d6124ae9b27dac180612942a3f6b
Ilya <ilya.soloveyv@gmail.com>
2018-09-16 01:17:25 +0300

    add pug

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const pug = require('pug')
 
-app.get('/', (req, res) => res.send('Shuttle. Test page'))
-app.get('/test', (req, res) => res.send('Test page'))
+app.use(express.static('public'));
 
+app.set('view engine', 'pug');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/', (req, res) => {
+    res.render('index.pug', { title: 'Shuttle' })
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000'))
